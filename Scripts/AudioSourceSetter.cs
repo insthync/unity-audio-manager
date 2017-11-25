3c7a9f582661028ada147b4dd4ee3b39a01f1a20
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-11-25 19:47:36 +0700

    Add volume setting component

---
+++ Scripts/AudioSourceSetter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioSourceSetter : AudioComponent
+{
+    public AudioSource audioSource { get; private set; }
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = AudioManager.Singleton.GetVolumeLevel(SettingId);
+    }
+
+    private void Update()
+    {
+        audioSource.volume = AudioManager.Singleton.GetVolumeLevel(SettingId);
+    }
+}
