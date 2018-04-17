80a2e8e14d688c2b8392432f8cfd1a4fa2dd3db8
insthync <ittipon.bay@gmail.com>
2018-04-17 10:10:01 +0700

    Improve audio source setter now it's not require Audio Source component

--- Scripts/AudioSourceSetter.cs
+++ Scripts/AudioSourceSetter.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
 public class AudioSourceSetter : AudioComponent
 {
     public enum PlayMode
@@ -13,19 +12,18 @@ public class AudioSourceSetter : AudioComponent
     public PlayMode playMode;
     public bool playOnAwake = true;
     public AudioClip[] randomClips;
-    private AudioSource tempAudioSource;
-    public AudioSource TempAudioSource
-    {
-        get
-        {
-            if (tempAudioSource == null)
-                tempAudioSource = GetComponent<AudioSource>();
-            return tempAudioSource;
-        }
-    }
+    private AudioSource cacheAudioSource;
+
     private void Awake()
     {
-        TempAudioSource.Stop();
+        if (playMode == PlayMode.PlayClipAtAudioSource)
+        {
+            cacheAudioSource = GetComponent<AudioSource>();
+            if (cacheAudioSource == null)
+                cacheAudioSource = gameObject.AddComponent<AudioSource>();
+            cacheAudioSource.Stop();
+        }
+
         if (playOnAwake)
             Play();
     }
@@ -36,20 +34,21 @@ public class AudioSourceSetter : AudioComponent
         if (randomClips.Length > 0)
             clip = randomClips[Random.Range(0, randomClips.Length - 1)];
 
+        // No random clips, try to use clip from audio source
         if (clip == null)
         {
-            if (TempAudioSource.clip == null)
+            if (cacheAudioSource.clip == null)
                 return;
-            clip = TempAudioSource.clip;
+            clip = cacheAudioSource.clip;
         }
 
         var volume = AudioManager.Singleton.GetVolumeLevel(SettingId);
         switch (playMode)
         {
             case PlayMode.PlayClipAtAudioSource:
-                TempAudioSource.clip = clip;
-                TempAudioSource.volume = volume;
-                TempAudioSource.Play();
+                cacheAudioSource.clip = clip;
+                cacheAudioSource.volume = volume;
+                cacheAudioSource.Play();
                 break;
             case PlayMode.PlayClipAtPoint:
                 AudioSource.PlayClipAtPoint(clip, transform.position, volume);
@@ -59,14 +58,19 @@ public class AudioSourceSetter : AudioComponent
 
     private void Update()
     {
-        TempAudioSource.volume = AudioManager.Singleton.GetVolumeLevel(SettingId);
+        if (playMode == PlayMode.PlayClipAtAudioSource)
+            cacheAudioSource.volume = AudioManager.Singleton.GetVolumeLevel(SettingId);
     }
 
 #if UNITY_EDITOR
     private void OnValidate()
     {
-        TempAudioSource.playOnAwake = false;
-        TempAudioSource.volume = 0;
+        var audioSource = GetComponent<AudioSource>();
+        if (audioSource != null)
+        {
+            audioSource.playOnAwake = false;
+            audioSource.volume = 0;
+        }
     }
 #endif
 }
