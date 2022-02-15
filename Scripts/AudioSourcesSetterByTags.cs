62d651da98c6a25e10e6232fca1c82b37748a356
Insthync <ittipon.bay@gmail.com>
2022-02-15 15:09:52 +0700

    Add `AudioSourcesSetterByTags` component, this component will find for audio source and add audio source setter component into its game object.

---
+++ Scripts/AudioSourcesSetterByTags.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourcesSetterByTags : MonoBehaviour
+{
+    public enum NoTagSetting
+    {
+        Bgm,
+        Sfx,
+        Ambient,
+    }
+
+    [System.Serializable]
+    public struct CustomIdTag
+    {
+        public string tag;
+        public string id;
+    }
+
+    public NoTagSetting noTagSetting = NoTagSetting.Sfx;
+    public string bgmTag = "BGM";
+    public string sfxTag = "SFX";
+    public string ambientTag = "AMBIENT";
+    public CustomIdTag[] customIdTags = new CustomIdTag[0];
+
+    private void Start()
+    {
+        Dictionary<string, string> tempCustomIdTags = new Dictionary<string, string>();
+        foreach (CustomIdTag customIdTag in customIdTags)
+        {
+            tempCustomIdTags[customIdTag.tag] = customIdTag.id;
+        }
+        AudioSourceSetter tempComp;
+        AudioSource[] sources = FindObjectsOfType<AudioSource>(true);
+        foreach (AudioSource source in sources)
+        {
+            tempComp = source.gameObject.GetComponent<AudioSourceSetter>();
+            if (tempComp) continue;
+            tempComp = source.gameObject.AddComponent<AudioSourceSetter>();
+            if (tempComp.gameObject.CompareTag(bgmTag))
+            {
+                tempComp.type = AudioComponentSettingType.Bgm;
+            }
+            else if (tempComp.gameObject.CompareTag(sfxTag))
+            {
+                tempComp.type = AudioComponentSettingType.Sfx;
+            }
+            else if (tempComp.gameObject.CompareTag(ambientTag))
+            {
+                tempComp.type = AudioComponentSettingType.Ambient;
+            }
+            else if (tempCustomIdTags.ContainsKey(tempComp.gameObject.tag))
+            {
+                tempComp.type = AudioComponentSettingType.Other;
+                tempComp.otherSettingId = tempCustomIdTags[tempComp.gameObject.tag];
+            }
+            else
+            {
+                switch (noTagSetting)
+                {
+                    case NoTagSetting.Bgm:
+                        tempComp.type = AudioComponentSettingType.Bgm;
+                        break;
+                    case NoTagSetting.Sfx:
+                        tempComp.type = AudioComponentSettingType.Sfx;
+                        break;
+                    case NoTagSetting.Ambient:
+                        tempComp.type = AudioComponentSettingType.Ambient;
+                        break;
+                }
+            }
+        }
+    }
+}
