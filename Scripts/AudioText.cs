ddb8e7608c1f6b1b3a252c03c21ca8dd893ace33
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-04-29 09:19:24 +0700

    Add audio text component

---
+++ Scripts/AudioText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class AudioText : AudioComponent
+{
+    public Text text { get; private set; }
+    public string format = "{0}%";
+
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        text.text = string.Format(format, AudioManager.Singleton.GetVolumeLevelSetting(SettingId));
+    }
+}
