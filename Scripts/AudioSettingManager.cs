87e2336b8ee98be89b183cd4a309ef93368ad813
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-09-07 16:00:14 +0700

    improve: change variable names

--- Scripts/AudioSettingManager.cs
+++ Scripts/AudioSettingManager.cs
@@ -4,37 +4,37 @@ using UnityEngine;
 [DefaultExecutionOrder(100)]
 public class AudioSettingManager : MonoBehaviour
 {
-    private Dictionary<string, AudioSetting> defaultVolumeSettings = new Dictionary<string, AudioSetting>();
-    private Dictionary<string, AudioSlider> sliders = new Dictionary<string, AudioSlider>();
-    private Dictionary<string, AudioToggle> toggles = new Dictionary<string, AudioToggle>();
+    private Dictionary<string, AudioSetting> _defaultVolumeSettings = new Dictionary<string, AudioSetting>();
+    private Dictionary<string, AudioSlider> _sliders = new Dictionary<string, AudioSlider>();
+    private Dictionary<string, AudioToggle> _toggles = new Dictionary<string, AudioToggle>();
 
     private void OnEnable()
     {
-        defaultVolumeSettings.Clear();
+        _defaultVolumeSettings.Clear();
         foreach (KeyValuePair<string, AudioSetting> kv in AudioManager.Singleton.VolumeSettings)
         {
-            defaultVolumeSettings[kv.Key] = kv.Value.Clone();
+            _defaultVolumeSettings[kv.Key] = kv.Value.Clone();
         }
         AudioSlider[] sliderComps = GetComponentsInChildren<AudioSlider>(true);
         foreach (AudioSlider sliderComp in sliderComps)
         {
-            sliders[sliderComp.SettingId] = sliderComp;
+            _sliders[sliderComp.SettingId] = sliderComp;
         }
         AudioToggle[] toggleComps = GetComponentsInChildren<AudioToggle>(true);
         foreach (AudioToggle toggleComp in toggleComps)
         {
-            toggles[toggleComp.SettingId] = toggleComp;
+            _toggles[toggleComp.SettingId] = toggleComp;
         }
     }
 
     public void ResetAudioSetting()
     {
-        foreach (var kv in defaultVolumeSettings)
+        foreach (var kv in _defaultVolumeSettings)
         {
-            if (sliders.ContainsKey(kv.Key))
-                sliders[kv.Key].slider.SetValueWithoutNotify(kv.Value.LevelSetting);
-            if (toggles.ContainsKey(kv.Key))
-                toggles[kv.Key].toggle.SetIsOnWithoutNotify(kv.Value.IsOn);
+            if (_sliders.ContainsKey(kv.Key))
+                _sliders[kv.Key].slider.SetValueWithoutNotify(kv.Value.LevelSetting);
+            if (_toggles.ContainsKey(kv.Key))
+                _toggles[kv.Key].toggle.SetIsOnWithoutNotify(kv.Value.IsOn);
             AudioManager.Singleton.VolumeSettings[kv.Key] = kv.Value.Clone();
         }
     }
